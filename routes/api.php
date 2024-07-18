c25ea1034906954731392707fb41002e9824a7dc
Marcelo Gadotti <eng.marcelogadotti@gmail.com>
2024-07-18 20:57:55 -0300

    chore: update routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,29 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function () {
-    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/loginapi', [AuthController::class, 'loginApi']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::group([
+        "middleware" => "auth:api"
+    ], function () {
+        // needs the bearer token
+        // url/api/auth/upload
+        Route::post('/upload', [FileController::class, 'imageUpload']);
+    });
+
+});
+
+Route::group([
+    'middleware' => 'api',
+], function () {
+    Route::group([
+        "middleware" => "auth:api"
+    ], function () {
+        // needs the bearer token
+        // url/api/upload
+        Route::post('/upload', [FileController::class, 'imageUpload']);
+        Route::post('/upload_file', [FileController::class, 'fileUpload']);
+    });
+
 });
